88b7f534850dc66cd25086fa511c5dc7a79f9315
Manon VAVASSEUR <Manon.vavasseur@live.fr>
2021-12-17 16:06:36 +0100

    burger finit

--- script.js
+++ script.js
@@ -1,18 +1,13 @@
 const hamburger = document.querySelector(".hamburger")
 const blocUl = document.querySelector(".bloc-ul")
-const navLink = document.querySelectorAll(".nav-link")
 const logo = document.querySelector(".containerlogo")
 
 
 hamburger.addEventListener("click", function () {
-    if (hamburger.style.display === "block") {
-        logo.style.display = "none" 
-        blocUl.style.display = "flex"
-    }
-    else {
-        hamburger.style.display = "block"
-        logo.style.display = "block"
-        blocUl.style.display = "none"
-    }
+     blocUl.classList.toggle("display-flex")
+     blocUl.classList.toggle("display-none")
+
+     logo.classList.toggle("display-flex")
+     logo.classList.toggle("display-none")
 })
 
